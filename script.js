e34ef58543a8b4655b9be45155d607d39fea9561
bobur <boburmakhmadkulov05@gmail.com>
2023-10-19 11:18:24 +0500

    second commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function proportion(total, broken) {
     console.log("The persontage:", (broken * 100 / total).toFixed(2));
 }
 
-let sent_cars = 3450000
+let total_cars = 3450000
 let broken_cars = 543245
 
-proportion(sent_cars, broken_cars)
+proportion(total_cars, broken_cars)
