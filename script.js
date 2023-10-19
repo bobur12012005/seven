03285b48393c353e76fb6b3b27936179528a172c
bobur <boburmakhmadkulov05@gmail.com>
2023-10-19 14:45:44 +0500

    third commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function proportion(total, broken) {
-    console.log("The persontage:", (broken * 100 / total).toFixed(2));
+    console.log("The persontage:", (broken * 100 / total).toFixed(2) + "%");
 }
 
 let total_cars = 3450000
